b10c3dfa16cd9d987f50f765db474d5d35a1ec89
german-moreno-rojas <german305350@gmail.com>
2025-09-24 10:49:19 -0500

    LOGIN-responsive

--- src/app/layoutClient.jsx
+++ src/app/layoutClient.jsx
@@ -5,14 +5,18 @@ import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 
 export default function LayoutClient({ children }) {
-  const pathname = usePathname();
-  const showHeader = pathname !== "/";
 
+  const hiddenPaths = [
+    '/login'
+  ]
+
+  const pathname = usePathname();
+  const showHeader = hiddenPaths.includes(pathname)
   return (
     <div className="min-h-screen flex flex-col">
-      {showHeader && <Header />}
+      {!showHeader && <Header />}
       <main className="flex-grow">{children}</main>
-      <Footer />
+      { !showHeader && <Footer /> }
     </div>
   );
 }
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -14,45 +14,45 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <div className="flex w-full max-w-4xl bg-white shadow-lg rounded-2xl overflow-hidden">
+    <div className="w-full h-[100vh] lg:min-h-screen flex lg:items-center justify-center bg-gray-50">
+      <div className="flex flex-col lg:flex-row w-full lg:max-w-4xl lg:justify-center lg:items-center bg-white shadow-lg lg:rounded-2xl overflow-hidden">
         {/* Imagen o logo */}
-        <div className="hidden md:flex w-1/2 bg-gray-200 items-center justify-center">
-          <span className="text-xl font-bold text-black-600">IMAGEN</span>
+        <div className="flex w-full h-full object-cover  lg:w-1/2 lg:h-1/2 bg-gray-200 lg:items-center lg:justify-center lg:mx-10 lg:rounded-2xl">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-FK_TmjhF_ssJyNDdeKRG2a6vGp5hstgThw&s" alt="tienda_ropa" className="w-full h-full lg:rounded-2xl" />
         </div>
 
         {/* Formulario */}
         <div className="w-full md:w-1/2 p-8">
-          <h1 className="text-2xl font-bold mb-2 text-center">Iniciar Sesión</h1>
-          <p className="text-black-500 mb-6 text-center">
+          <h1 className="text-2xl text-gray-800 font-bold mb-2 text-center">Iniciar Sesión</h1>
+          <p className="text-gray-800 mb-6 text-center">
             Bienvenido, por favor ingresa tus datos para continuar
           </p>
 
           <form onSubmit={handleSubmit}>
             {/* Usuario */}
-            <label className="block mb-2">Correo Electrónico</label>
+            <label className="block mb-2 text-gray-800">Correo Electrónico</label>
             <input
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full p-2 border rounded mb-4"
+              className="w-full p-2 border rounded mb-4 text-gray-800"
               required
             />
 
             {/* Contraseña con mostrar/ocultar */}
-            <label className="block mb-2">Contraseña</label>
+            <label className="block mb-2 text-gray-800">Contraseña</label>
             <div className="relative mb-4">
               <input
                 type={showPassword ? "text" : "password"}
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="w-full p-2 border rounded pr-10"
+                className="w-full p-2 border rounded pr-10 text-gray-800"
                 required
               />
               <button
                 type="button"
                 onClick={() => setShowPassword(!showPassword)}
-                className="absolute inset-y-0 right-2 flex items-center text-gray-500"
+                className="absolute inset-y-0 right-2 flex items-center text-gray-800"
               >
                 {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
               </button>
@@ -76,14 +76,14 @@ export default function LoginPage() {
 
           {/* Ingreso con Google */}
           <div className="mt-6">
-            <button className="w-full border p-2 rounded flex items-center justify-center gap-2 hover:bg-gray-100">
+            <button className="w-full border p-2 text-gray-800 rounded flex items-center justify-center gap-2 hover:bg-gray-100">
               <img
                 src="https://www.svgrepo.com/show/355037/google.svg"
                 alt="Google"
-                className="w-5 h-5"
+                className="w-5 h-5 text-gray-800"
               />
               Ingresar con Google
-            </button>
+            </button >
           </div>
 
           {/* Registrarse */}
